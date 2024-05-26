a9c23a6d30d99197679f74b0c7750c1eaac0130b
Ian Garraway <IJGarraway@gmail.com>
2024-05-26 20:43:25 +0100

    activity 3 complete

---
+++ activity-files/IntroductionToReactJS/vite-project/src/AnotherComponent.jsx
@@ -0,0 +1,9 @@
+const AnotherComponent = () => {
+    return (
+        <>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, eum! </p>
+        </>
+    )
+}
+
+export default AnotherComponent
