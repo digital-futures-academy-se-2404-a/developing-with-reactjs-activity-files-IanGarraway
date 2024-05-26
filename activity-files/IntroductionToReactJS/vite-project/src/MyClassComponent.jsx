99c71aae7786a2ae21e5d6e3876a9fa22276faf0
Ian Garraway <IJGarraway@gmail.com>
2024-05-26 20:47:58 +0100

    Activity 4

---
+++ activity-files/IntroductionToReactJS/vite-project/src/MyClassComponent.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class MyClassComponent extends Component{
+
+    render() {
+        return (
+            <>
+                <h2>Lorem ipsum dolor sit amet.</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae facilis doloribus necessitatibus nesciunt vel, expedita blanditiis nihil laborum, architecto est totam dignissimos sit, qui vero.</p>
+            </>
+        )
+    }
+
+}
+
+
+export default MyClassComponent
--- activity-files/IntroductionToReactJS/vite-project/src/MyComponent.jsx
+++ activity-files/IntroductionToReactJS/vite-project/src/MyComponent.jsx
@@ -1,10 +1,12 @@
 import AnotherComponent from "./AnotherComponent.jsx";
+import MyClassComponent from "./MyClassComponent.jsx";
 
 const MyComponent = () => {
     return (
         <>
             <h1>Hello World!</h1>
             <AnotherComponent />
+            <MyClassComponent />
         </>
     );
 }
